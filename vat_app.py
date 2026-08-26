fba5ade55ebe56b2c78e27756c6aadd055621cf2
417-05-waratkon <54228@yupparaj.ac.th>
2026-08-26 11:26:54 +0700

    Create vat_app.py

---
+++ vat_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("🛒 แอปพลิเคชันคำนวณราคาสินค้ารวม VAT 7%")
+
+price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+
+vat = price * 0.07
+net_price = price - vat
+
+st.header(f"ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+
+st.write("นายวรัตม์คล อ้นภากุลวิวัฒน์ เลขที่ 5 ม.4/17")
+
+st.divider()
+
+st.header(f"ราคาสุทธิ: {net_price:.2f} บาท")

fd56d6d2a0113b65ad853d460760eee3ac111049
417-05-waratkon <54228@yupparaj.ac.th>
2026-09-09 11:49:27 +0700

    vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -58,7 +58,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
     # ตรวจข้อ 4 (เพิ่มใหม่)
-    if u_ans4 == "dog":
+    if u_ans4 == "Cherry":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
@@ -104,7 +104,7 @@ ans3 = st.text_input(
     value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
-    "ข้อ 4: A d _ g is a human's best friend. 🐶",
+    "ข้อ 4: Ch_ _ _ y is my favorite fruit. 🍒",
     value=st.session_state.ans4_val,
 )
 
